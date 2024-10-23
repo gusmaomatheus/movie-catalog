d377cb28f0b07238f29e40b4fe6ab30e267daffe
Matheus Gusmão <mgusmao.dev@gmail.com>
2024-10-23 17:25:07 -0300

    feat(api): create axios instance for movie API
    - set baseUrl to mock API for movies
    - added 1000ms timeout and content-type header as JSON

---
+++ infra/api.js
@@ -0,0 +1,9 @@
+import axios from "axios";
+
+export const api = axios.create({
+    baseURL: "https://671959c87fc4c5ff8f4d4379.mockapi.io/movies",
+    timeout: 1000,
+    headers: {
+        "Content-Type": "application/json",
+    },
+});
