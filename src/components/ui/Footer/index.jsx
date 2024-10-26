3d429b16b1b34479effdf15b0554acf803c24190
Matheus Gusmão <mgusmao.dev@gmail.com>
2024-10-26 01:38:58 -0300

    feat: create Footer component to represents the footer of application

---
+++ src/components/ui/Footer/index.jsx
@@ -0,0 +1,7 @@
+export default function Footer() {
+    return (
+        <footer className="fixed bottom-4 w-full text-center text-emerald-400 text-[18px]">
+            Developed with &#9829; by Matheus Gusmão
+        </footer>
+    );
+}
