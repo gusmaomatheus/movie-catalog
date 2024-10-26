33343ecf6af0249094923ed1745ce235ca39d93e
Matheus Gusmão <mgusmao.dev@gmail.com>
2024-10-26 00:00:05 -0300

    feat(routes): add route for header component

--- src/routes/AppRouter.jsx
+++ src/routes/AppRouter.jsx
@@ -1,9 +1,11 @@
 import { BrowserRouter, Route, Routes } from "react-router-dom";
 import Home from "../components/pages/Home/";
+import Header from "../components/ui/Header";
 
 export default function AppRouter() {
     return (
         <BrowserRouter>
+            <Header />
             <Routes>
                 <Route path="/" element={<Home />} />
             </Routes>
