3992b826c499718f81b085221901b6167b8107fd
Matheus Gusmão <mgusmao.dev@gmail.com>
2024-10-26 00:54:25 -0300

    feat: change way of handling of the axios instance on MovieService

--- src/services/MovieService.js
+++ src/services/MovieService.js
@@ -1,6 +1,6 @@
 import axios from "axios";
 
-const api = axios.create({
+export const api = axios.create({
     baseURL: "https://671959c87fc4c5ff8f4d4379.mockapi.io/movies",
     timeout: 1000,
     headers: {
