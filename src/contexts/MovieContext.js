d71db382ef9bd782f24f27fea9e9ec9dd2113c71
Matheus Gusmão <mgusmao.dev@gmail.com>
2024-10-26 00:44:31 -0300

    feat: create MovieContext for managment global states

---
+++ src/contexts/MovieContext.js
@@ -0,0 +1,9 @@
+import { createContext, useState } from "react";
+
+export const MovieContext = createContext();
+
+export const MovieProvider = ({ children }) => {
+    const [movies, setMovies] = useState([]);
+
+    return <MovieContext.Provider value={{ movies, setMovies }}>{children}</MovieContext.Provider>;
+};
