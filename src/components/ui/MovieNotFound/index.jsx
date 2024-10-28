b7505a1f84f6a2900b1e82853b13d619e9cd2c9d
Matheus Gusmão <mgusmao.dev@gmail.com>
2024-10-28 07:46:37 -0300

    feat(DeleteMovie): add logic for components used on DeleteMovie page

--- src/components/ui/MovieNotFound/index.jsx
+++ src/components/ui/MovieNotFound/index.jsx
@@ -1,11 +1,17 @@
 import Button from "../Button";
 
-export default function MovieNotFound() {
+export default function MovieNotFound({ setIsSearchingMovie }) {
+    const cancel = () => {
+        setIsSearchingMovie(false);
+    };
+
     return (
-        <div className="flex flex-col items-center bg-red-100 gap-y-[32px] border-[1px] border-solid border-red-200 rounded-md">
+        <div className="flex flex-col items-center bg-red-200 gap-y-[32px] border-[1px] border-solid border-red-300 rounded-md">
             <div className="flex flex-col mx-[32px] my-[32px] place-items-center gap-y-[32px]">
                 <p className="text-center text-red-600 text-[28px]">Movie not found.</p>
-                <Button color="#fca5a5">Try Again</Button>
+                <Button color="#fca5a5" onClick={cancel}>
+                    Try Again
+                </Button>
             </div>
         </div>
     );
--- src/components/ui/SearchMovie/index.jsx
+++ src/components/ui/SearchMovie/index.jsx
@@ -1,19 +1,44 @@
+import { useContext, useState } from "react";
 import { useNavigate } from "react-router-dom";
+import { MovieContext } from "../../../contexts/MovieContext";
 import Button from "../Button";
 import TextInput from "../TextInput";
 
-export default function SearchMovie() {
+export default function SearchMovie({ setMovie, isSearchingMovie, setIsSearchingMovie }) {
     const navigate = useNavigate();
+
+    const { movies } = useContext(MovieContext);
+    const [movieId, setMovieId] = useState("");
+
+    const findMovie = () => {
+        if (!movieId) return;
+
+        const movie = movies.find(movie => movie.id == movieId);
+
+        setMovie(movie ? movie : null);
+        setIsSearchingMovie(true);
+        onChangeMovieId("");
+    };
+
+    const onChangeMovieId = value => {
+        setMovieId(value);
+    };
 
     const navigateToHome = () => {
         navigate("/");
+        setIsSearchingMovie(false);
+        setMovieId("");
+        setMovie(null);
     };
+
     return (
         <form className="flex flex-col gap-y-[32px]">
             <p className="text-black text-[32px] text-center mt-[32px] font-medium uppercase">search movie</p>
-            <TextInput label="Movie id" placeholder="1" />
+            <TextInput label="Movie id" placeholder="1" value={movieId} onChange={onChangeMovieId} />
             <div className="flex gap-x-[16px] self-center">
-                <Button color="#7dd3fc">Search</Button>
+                <Button color="#7dd3fc" disabled={isSearchingMovie} onClick={findMovie}>
+                    Search
+                </Button>
                 <Button color="#fca5a5" onClick={navigateToHome}>
                     Cancel
                 </Button>
