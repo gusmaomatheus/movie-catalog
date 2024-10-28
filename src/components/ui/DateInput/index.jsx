b76eba60493161df01541ad7374feb24a7c44944
Matheus Gusmão <mgusmao.dev@gmail.com>
2024-10-27 23:10:21 -0300

    feat(components/ui): implements onChange callback function for input components

--- src/components/ui/DateInput/index.jsx
+++ src/components/ui/DateInput/index.jsx
@@ -3,6 +3,10 @@ import { useId } from "react";
 export default function DateInput({ label, ref }) {
     const id = useId();
 
+    const handleInputChange = event => {
+        ref.current.value = event.target.value;
+    };
+
     return (
         <div className="flex flex-col relative">
             <label htmlFor={id} className="absolute top-[-10px] left-[12px] bg-white px-[4px] text-black text-[16px]">
@@ -14,6 +18,7 @@ export default function DateInput({ label, ref }) {
                 id={id}
                 ref={ref}
                 className="w-[360px] px-[12px] pt-[12px] pb-[4px] border-[1px] border-solid border-gray-300 rounded-md text-gray-600 text-[18px] focus:outline-none"
+                onChange={handleInputChange}
             />
         </div>
     );
--- src/components/ui/TextInput/index.jsx
+++ src/components/ui/TextInput/index.jsx
@@ -3,6 +3,10 @@ import { useId } from "react";
 export default function TextInput({ label, placeholder, ref }) {
     const id = useId();
 
+    const handleInputChange = event => {
+        ref.current.value = event.target.value;
+    };
+
     return (
         <div className="flex flex-col relative">
             <label htmlFor={id} className="absolute top-[-10px] left-[12px] bg-white px-[4px] text-black text-[16px]">
@@ -15,6 +19,7 @@ export default function TextInput({ label, placeholder, ref }) {
                 placeholder={placeholder}
                 ref={ref}
                 className="w-[360px] px-[12px] pt-[12px] pb-[4px] border-[1px] border-solid border-gray-300 rounded-md text-gray-600 text-[18px] focus:outline-none"
+                onChange={handleInputChange}
             />
         </div>
     );
