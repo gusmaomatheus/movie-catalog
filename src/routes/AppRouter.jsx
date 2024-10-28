2e64cbdc5679c65dab9df777f66bf20173a5ac9b
Matheus Gusmão <mgusmao.dev@gmail.com>
2024-10-27 23:57:29 -0300

    feat(routes): create new route for CreateMovie page

--- src/routes/AppRouter.jsx
+++ src/routes/AppRouter.jsx
@@ -1,4 +1,5 @@
 import { BrowserRouter, Route, Routes } from "react-router-dom";
+import CreateMovie from "../components/pages/CreateMovie";
 import Home from "../components/pages/Home/";
 import MovieDetais from "../components/pages/MovieDetails";
 import Footer from "../components/ui/Footer";
@@ -13,6 +14,7 @@ export default function AppRouter() {
                 <Routes>
                     <Route path="/" element={<Home />} />
                     <Route path="/details/:id" element={<MovieDetais />} />
+                    <Route path="/create" element={<CreateMovie />} />
                 </Routes>
                 <Footer />
             </MovieProvider>
