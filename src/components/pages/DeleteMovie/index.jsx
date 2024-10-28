13cf2360c830c1d04c88077013e426a690aca95f
Matheus Gusmão <mgusmao.dev@gmail.com>
2024-10-28 07:45:53 -0300

    feat(components/pages): create DeleteMovie page
    - delete any movie by your id

---
+++ src/components/pages/DeleteMovie/index.jsx
@@ -0,0 +1,30 @@
+import { useState } from "react";
+import MovieManagement from "../../ui/MovieManagement";
+import MovieNotFound from "../../ui/MovieNotFound";
+import SearchMovie from "../../ui/SearchMovie";
+
+export default function DeleteMovie() {
+    const [movie, setMovie] = useState(null);
+    const [isSearchingMovie, setIsSearchingMovie] = useState(false);
+
+    return (
+        <main className="flex flex-col items-center">
+            <SearchMovie
+                setMovie={setMovie}
+                isSearchingMovie={isSearchingMovie}
+                setIsSearchingMovie={setIsSearchingMovie}
+            />
+            <div className="mt-[64px]">
+                {isSearchingMovie ? (
+                    movie ? (
+                        <MovieManagement type="delete" movie={movie} setIsSearchingMovie={setIsSearchingMovie} />
+                    ) : (
+                        <MovieNotFound setIsSearchingMovie={setIsSearchingMovie} />
+                    )
+                ) : (
+                    ""
+                )}
+            </div>
+        </main>
+    );
+}
