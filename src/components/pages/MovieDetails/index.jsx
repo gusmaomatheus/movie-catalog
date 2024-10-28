9a3925653db1a15cd97b0fff2803b5ccdd461049
Matheus Gusmão <mgusmao.dev@gmail.com>
2024-10-27 21:22:39 -0300

    refactor(MovieDetails): change way of the movie infos is renderized

--- src/components/pages/MovieDetails/index.jsx
+++ src/components/pages/MovieDetails/index.jsx
@@ -2,28 +2,12 @@ import { useContext } from "react";
 import { useNavigate, useParams } from "react-router-dom";
 import { MovieContext } from "../../../contexts/MovieContext";
 
-export default function MovieDetais() {
+export default function MovieDetails() {
     const { movies } = useContext(MovieContext);
     const { id } = useParams();
     const navigate = useNavigate();
 
-    const renderDetailsInfos = () => {
-        const movie = movies.find(m => m.id === id);
-
-        return (
-            <div className="flex flex-col gap-y-[8px] ms-[48px] mt-[64px]">
-                <p className="text-black text-[20px]">
-                    <span className="font-semibold">Name:</span> {movie.name}
-                </p>
-                <p className="text-black text-[20px]">
-                    <span className="font-semibold">Gender:</span> {movie.gender}
-                </p>
-                <p className="text-black text-[20px]">
-                    <span className="font-semibold">Year:</span> {movie.dateOfRelease}
-                </p>
-            </div>
-        );
-    };
+    const movie = movies.find(m => m.id === id);
 
     const handleNavigateBack = () => {
         navigate("/");
@@ -32,7 +16,21 @@ export default function MovieDetais() {
     return (
         <main className="flex flex-col place-self-center justify-self-center w-[900px] h-[400px] mt-[160px] shadow-sm border-[1px] border-solid border-gray-200">
             <p className="text-black text-[32px] text-center mt-[32px] font-medium">MOVIE DETAILS:</p>
-            {renderDetailsInfos()}
+            {movie ? (
+                <div className="flex flex-col gap-y-[8px] ms-[48px] mt-[64px]">
+                    <p className="text-black text-[20px]">
+                        <span className="font-semibold">Name:</span> {movie.name}
+                    </p>
+                    <p className="text-black text-[20px]">
+                        <span className="font-semibold">Gender:</span> {movie.gender}
+                    </p>
+                    <p className="text-black text-[20px]">
+                        <span className="font-semibold">Year:</span> {movie.dateOfRelease}
+                    </p>
+                </div>
+            ) : (
+                <p className="text-red-500 text-center mt-[32px]">Movie not found</p>
+            )}
             <button
                 type="button"
                 className="w-[180px] h-[40px] mt-[64px] bg-red-200 rounded-md text-white text-[20px] self-center transition duration-300 ease-in-out hover:bg-red-400 hover:scale-105"
