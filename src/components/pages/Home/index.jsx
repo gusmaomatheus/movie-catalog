0c987bf6021dfbe416396ae4f15e1d50f5daab36
Matheus Gusmão <mgusmao.dev@gmail.com>
2024-10-27 21:45:27 -0300

    feat: add rendering for when movies haven't loaded yet

--- src/components/pages/Home/index.jsx
+++ src/components/pages/Home/index.jsx
@@ -1,16 +1,21 @@
-import { useContext, useEffect } from "react";
+import { faCircleNotch } from "@fortawesome/free-solid-svg-icons";
+import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
+import { useContext, useEffect, useState } from "react";
 import { MovieContext } from "../../../contexts/MovieContext";
 import { api } from "../../../services/MovieService";
 import MovieCard from "../../ui/MovieCard";
 
 export default function Home() {
     const { movies, setMovies } = useContext(MovieContext);
+    const [isDataFetched, setIsDataFetched] = useState(false);
 
     useEffect(() => {
         fetchAll();
     }, []);
 
     async function fetchAll() {
+        setIsDataFetched(true);
+
         try {
             const response = await api.get();
 
@@ -19,6 +24,8 @@ export default function Home() {
             }
         } catch (error) {
             console.error(error);
+        } finally {
+            setIsDataFetched(false);
         }
     }
 
@@ -26,7 +33,9 @@ export default function Home() {
         <main className="flex flex-col gap-y-[64px] mb-[32px]">
             <p className="text-emerald-400 text-[48px] text-center upperace">MOVIES CATALOG</p>
             <div className="flex flex-col items-center w-[100%] px-[256px] gap-y-[16px] ">
-                {movies.length > 0 ? (
+                {isDataFetched ? (
+                    <FontAwesomeIcon className="text-emerald-400 mt-[120px]" icon={faCircleNotch} spin size="2xl" />
+                ) : movies.length > 0 ? (
                     movies.map(movie => {
                         return <MovieCard key={movie.id} movie={movie} />;
                     })
