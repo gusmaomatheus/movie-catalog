39c582ef3688dd8bc83a64ad4ed4dea72112888c
Matheus Gusmão <mgusmao.dev@gmail.com>
2024-10-28 12:40:25 -0300

    feat(MovieManagment): add logic for update movie

--- src/components/ui/MovieManagement/index.jsx
+++ src/components/ui/MovieManagement/index.jsx
@@ -2,11 +2,65 @@ import { useState } from "react";
 import Swal from "sweetalert2";
 import { api } from "../../../services/MovieService";
 import Button from "../Button";
+import MovieUpdateForm from "../MovieUpdateForm";
 
 export default function MovieManagement({ type = "update", movie, setIsSearchingMovie }) {
     const [isExecuting, setIsExecuting] = useState(false);
+    const [name, setName] = useState("");
+    const [gender, setGender] = useState("");
+    const [year, setYear] = useState("");
 
-    const updateMovie = () => {};
+    async function updateMovie() {
+        setIsExecuting(true);
+        try {
+            if (!isValid()) {
+                Swal.fire({
+                    toast: true,
+                    position: "bottom-end",
+                    icon: "error",
+                    text: "Invalid form information",
+                    showConfirmButton: false,
+                    timer: 2000,
+                    timerProgressBar: true,
+                });
+
+                return;
+            }
+
+            const data = {
+                name: name,
+                gender: gender,
+                dateOfRelease: year,
+            };
+
+            const response = await api.put(`/${movie.id}`, data);
+
+            if (response.status === 200) {
+                Swal.fire({
+                    toast: true,
+                    position: "bottom-end",
+                    icon: "success",
+                    text: "Movie updated successfully",
+                    showConfirmButton: false,
+                    timer: 2000,
+                    timerProgressBar: true,
+                });
+                setIsSearchingMovie(false);
+            }
+        } catch (error) {
+            Swal.fire({
+                toast: true,
+                position: "bottom-end",
+                icon: "error",
+                text: error.message,
+                showConfirmButton: false,
+                timer: 2000,
+                timerProgressBar: true,
+            });
+        } finally {
+            setIsExecuting(false);
+        }
+    }
 
     async function deleteMovie() {
         setIsExecuting(true);
@@ -44,22 +98,44 @@ export default function MovieManagement({ type = "update", movie, setIsSearching
         setIsSearchingMovie(false);
     };
 
+    const isValid = () => {
+        if (!name) return false;
+        if (!gender) return false;
+        if (!year) return false;
+
+        return true;
+    };
+
     return (
-        <main className="flex flex-col place-self-center justify-self-center w-[800px] h-[330px] gap-y-[32px] shadow-sm border-[1px] border-solid border-gray-200">
+        <main className="flex flex-col place-self-center justify-self-center w-[800px] h-[400px] gap-y-[32px] shadow-sm border-[1px] border-solid border-gray-200">
             <p className="text-black text-[32px] text-center mt-[32px] font-medium">
                 {type === "update" ? "UPDATE MOVIE" : "DELETE MOVIE"}
             </p>
             {movie ? (
                 <div className="flex flex-col gap-y-[8px] ms-[48px]">
-                    <p className="text-black text-[20px]">
-                        <span className="font-semibold">Name:</span> {movie.name}
-                    </p>
-                    <p className="text-black text-[20px]">
-                        <span className="font-semibold">Gender:</span> {movie.gender}
-                    </p>
-                    <p className="text-black text-[20px]">
-                        <span className="font-semibold">Year:</span> {movie.dateOfRelease}
-                    </p>
+                    {type === "update" ? (
+                        <MovieUpdateForm
+                            movie={movie}
+                            name={name}
+                            setName={setName}
+                            gender={gender}
+                            setGender={setGender}
+                            year={year}
+                            setYear={setYear}
+                        />
+                    ) : (
+                        <div>
+                            <p className="text-black text-[20px]">
+                                <span className="font-semibold">Name:</span> {movie.name}
+                            </p>
+                            <p className="text-black text-[20px]">
+                                <span className="font-semibold">Gender:</span> {movie.gender}
+                            </p>
+                            <p className="text-black text-[20px]">
+                                <span className="font-semibold">Year:</span> {movie.dateOfRelease}
+                            </p>
+                        </div>
+                    )}
                 </div>
             ) : (
                 <p className="text-red-500 text-center mt-[32px]">Movie not found</p>
