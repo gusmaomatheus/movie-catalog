af225d775546a097b1864263b9d0ba75f4f30f37
Matheus Gusmão <mgusmao.dev@gmail.com>
2024-10-28 18:51:11 -0300

    feat(routes): modify routes for use Layout page

--- src/routes/AppRouter.jsx
+++ src/routes/AppRouter.jsx
@@ -2,27 +2,26 @@ import { BrowserRouter, Route, Routes } from "react-router-dom";
 import CreateMovie from "../components/pages/CreateMovie";
 import DeleteMovie from "../components/pages/DeleteMovie";
 import Home from "../components/pages/Home/";
+import Layout from "../components/pages/Layout";
 import MovieDetais from "../components/pages/MovieDetails";
 import NotFound from "../components/pages/NotFound";
 import UpdateMovie from "../components/pages/UpdateMovie";
-import Footer from "../components/ui/Footer";
-import Header from "../components/ui/Header";
 import { MovieProvider } from "../contexts/MovieContext";
 
 export default function AppRouter() {
     return (
         <BrowserRouter>
             <MovieProvider>
-                <Header />
                 <Routes>
-                    <Route path="*" element={<NotFound />} />
-                    <Route path="/" element={<Home />} />
-                    <Route path="/details/:id" element={<MovieDetais />} />
-                    <Route path="/create" element={<CreateMovie />} />
-                    <Route path="/update" element={<UpdateMovie />} />
-                    <Route path="/delete" element={<DeleteMovie />} />
+                    <Route element={<Layout />}>
+                        <Route path="/" element={<Home />} />
+                        <Route path="/details/:id" element={<MovieDetais />} />
+                        <Route path="/create" element={<CreateMovie />} />
+                        <Route path="/update" element={<UpdateMovie />} />
+                        <Route path="/delete" element={<DeleteMovie />} />
+                        <Route path="*" element={<NotFound />} />
+                    </Route>
                 </Routes>
-                <Footer />
             </MovieProvider>
         </BrowserRouter>
     );
