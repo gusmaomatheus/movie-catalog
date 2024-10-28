5338d06f7b6454a0cc2b756eb797eebb424186e3
Matheus Gusmão <mgusmao.dev@gmail.com>
2024-10-27 22:29:13 -0300

    feat(components/ui): create DateInput
    - represent custom input of type date

---
+++ src/components/ui/DateInput/index.jsx
@@ -0,0 +1,20 @@
+import { useId } from "react";
+
+export default function DateInput({ label, ref }) {
+    const id = useId();
+
+    return (
+        <div className="flex flex-col relative">
+            <label htmlFor={id} className="absolute top-[-10px] left-[12px] bg-white px-[4px] text-black text-[16px]">
+                {label}
+            </label>
+            <input
+                type="date"
+                name={id}
+                id={id}
+                ref={ref}
+                className="w-[360px] px-[12px] pt-[12px] pb-[4px] border-[1px] border-solid border-gray-300 rounded-md text-gray-600 text-[18px] focus:outline-none"
+            />
+        </div>
+    );
+}
