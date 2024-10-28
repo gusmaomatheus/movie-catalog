eebe150d511fdbb35f62484d2766cfe996114ede
Matheus Gusmão <mgusmao.dev@gmail.com>
2024-10-28 00:18:55 -0300

    feat(routes): implement specify component for not found route

--- src/routes/AppRouter.jsx
+++ src/routes/AppRouter.jsx
@@ -2,6 +2,7 @@ import { BrowserRouter, Route, Routes } from "react-router-dom";
 import CreateMovie from "../components/pages/CreateMovie";
 import Home from "../components/pages/Home/";
 import MovieDetais from "../components/pages/MovieDetails";
+import NotFound from "../components/pages/NotFound";
 import Footer from "../components/ui/Footer";
 import Header from "../components/ui/Header";
 import { MovieProvider } from "../contexts/MovieContext";
@@ -12,6 +13,7 @@ export default function AppRouter() {
             <MovieProvider>
                 <Header />
                 <Routes>
+                    <Route path="*" element={<NotFound />} />
                     <Route path="/" element={<Home />} />
                     <Route path="/details/:id" element={<MovieDetais />} />
                     <Route path="/create" element={<CreateMovie />} />
