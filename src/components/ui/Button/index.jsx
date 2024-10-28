8a889f8f58f00991fdf3a46f7f04b960196eb195
Matheus Gusmão <mgusmao.dev@gmail.com>
2024-10-27 22:58:49 -0300

    feat(Button): add 'disabled' prop to control the button state

--- src/components/ui/Button/index.jsx
+++ src/components/ui/Button/index.jsx
@@ -1,6 +1,7 @@
 export default function Button({
     children,
     color = "blue",
+    disabled = false,
     onClick = () => {
         return;
     },
@@ -11,6 +12,7 @@ export default function Button({
             className={`w-[120px] h-[40px] rounded-md text-white text-[18px] self-center transition duration-300 ease-in-out hover:scale-105`}
             style={{ backgroundColor: color }}
             onClick={onClick}
+            disabled={disabled}
         >
             {children}
         </button>
