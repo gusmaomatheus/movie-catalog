1e8666246ec3e4e9ab67510c28b0a35e07018609
Matheus Gusmão <mgusmao.dev@gmail.com>
2024-10-28 18:49:55 -0300

    feat(Footer): change styles for footer

--- src/components/ui/Footer/index.jsx
+++ src/components/ui/Footer/index.jsx
@@ -1,6 +1,6 @@
 export default function Footer() {
     return (
-        <footer className="fixed bottom-4 w-full text-center text-emerald-400 text-[18px]">
+        <footer className="mt-[16px] relative inset-x-0 bottom-0 text-center text-emerald-400 text-[18px] mb-4">
             Developed with &#9829; by Matheus Gusmão
         </footer>
     );
